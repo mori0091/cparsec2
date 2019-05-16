3821d5ee812453f768ff562dbb8f37057b39e748
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-05-16 21:42:53 +0900

    refactor Parser as struct pointer instead of function pointer

--- cparsec2.c
+++ cparsec2.c
@@ -1,55 +1,80 @@
 /* -*- coding:utf-8-unix -*- */
 #include <ctype.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdnoreturn.h>
 #include <string.h>
 
 #define UNUSED(v) ((void)v)
 
-/** Print error message then exit with error code. */
-noreturn void error(const char *fmt, ...) {
+/** Print error message and return '\0' */
+char error(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
-  fprintf(stderr, "\nerror:");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
+  printf("error:");
+  vprintf(fmt, ap);
+  printf("\n");
+  return '\0';
 }
 
-typedef struct stSource {
+typedef struct stSource *Source;
+struct stSource {
   const char *input; /* whole input */
   const char *p;     /* pointer to next char */
-} * Source;
+};
 
-typedef char (*Parser)(Source src);
+
+typedef struct stParser *Parser;
+struct stParser {
+  char (*run)(Parser self, Source src);
+};
+
+#define PARSE_TEST(p, input)                                                   \
+  printf("%8s \"%s\" => ", #p, input);                                         \
+  parseTest(p, input)
+
+char parse(Parser p, Source src) { return p->run(p, src); }
 
 void parseTest(Parser p, const char *input) {
   struct stSource src;
   src.input = input;
   src.p = input;
-  printf("\"%s\"", input);
-  char c = p(&src);
-  printf(" => '%c'\n", c);
+  char c = parse(p, &src);
+  if (c) {
+    printf("'%c'\n", c);
+  }
 }
 
-char anyChar(Source src) {
+extern Parser const anyChar;
+
+// anyChar
+
+static char run_anyChar(Parser p, Source src) {
+  UNUSED(p);
   char c = *src->p;
   if (!c) {
-    error("too short");
+    return error("too short");
   }
   return c;
 }
+
+static struct stParser anyChar_ = {
+    .run = run_anyChar,
+};
+
+Parser const anyChar = &anyChar_;
+
+// ----
 
 int main(int argc, char **argv) {
   UNUSED(argc);
   UNUSED(argv);
 
-  parseTest(anyChar, "abc"); /* a */
-  parseTest(anyChar, "bc");  /* b */
-  parseTest(anyChar, "123"); /* 1 */
-  parseTest(anyChar, "");    /* "error:too short" */
+  PARSE_TEST(anyChar, "abc"); /* a */
+  PARSE_TEST(anyChar, "bc");  /* b */
+  PARSE_TEST(anyChar, "123"); /* 1 */
+  PARSE_TEST(anyChar, "");    /* "error:too short" */
 
   return 0;
 }
