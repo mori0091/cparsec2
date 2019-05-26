d78dbf4e2545a0106509c86ff1f622d5ade70e02
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-05-26 14:15:23 +0900

    removed and ignored '.dir-locals.el' since it maybe cause mess
    - '.dir-locals.el' is a configuration file of Emacs text editor.
    - It should be specific to individual user not be to the project.
    - And ofcourse it is not needed for non-Emacs users.

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;; -*- coding: utf-8-unix -*-
-((nil . ((flycheck-clang-language-standard . "c11")
-         (flycheck-clang-include-path . ("."
-                                         "src"
-                                         "include"
-                                         ))
-         )
-      ))
