bd8919c8dd17b1aa5be8af1fb5f6c27e6bb824d3
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-05-27 22:24:35 +0900

    added new test SCENARIO("spaces")

---
+++ test/src/test_spaces.cpp
@@ -0,0 +1,41 @@
+/* -*- coding: utf-8-unix -*- */
+
+#include <catch.hpp>
+#include <cparsec2.hpp>
+
+SCENARIO("spaces", "[cparsec2][parser][spaces]") {
+  cparsec2_init();
+  GIVEN("an input: \"   a\"") {
+    Source src = Source_new("   a");
+    WHEN("apply spaces") {
+      THEN("results \"   \"") {
+        REQUIRE("   " == parse(spaces, src));
+      }
+    }
+  }
+  GIVEN("an input: \" \\t\\n\\r\" (i.e. SPC TAB LF CR)") {
+    Source src = Source_new(" \t\n\r");
+    WHEN("apply spaces") {
+      THEN("results \" \\t\\n\\r\"") {
+        REQUIRE(" \t\n\r" == parse(spaces, src));
+      }
+    }
+  }
+  GIVEN("an input: \"a\"") {
+    Source src = Source_new("a");
+    WHEN("apply spaces") {
+      THEN("results \"\"") {
+        REQUIRE("" == parse(spaces, src));
+      }
+    }
+  }
+  GIVEN("an input: \"\"") {
+    Source src = Source_new("");
+    WHEN("apply spaces") {
+      THEN("results \"\"") {
+        REQUIRE("" == parse(spaces, src));
+      }
+    }
+  }
+  cparsec2_end();
+}
