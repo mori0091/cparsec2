711926350950acd21d8e23ef108e39f2df5887a2
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-05-28 00:01:26 +0900

    added new test SCENARIO("string1(str)")

---
+++ test/src/test_string1.cpp
@@ -0,0 +1,50 @@
+/* -*- coding: utf-8-unix -*- */
+
+#include <catch.hpp>
+#include <cparsec2.hpp>
+
+SCENARIO("string1(str)", "[cparsec2][parser][string1]") {
+  cparsec2_init();
+  GIVEN("an input: \"a1234\"") {
+    Source src = Source_new("a1234");
+    WHEN("apply string1(\"a1234\")") {
+      THEN("results \"a1234\"") {
+        REQUIRE("a1234" == parse(string1("a1234"), src));
+      }
+    }
+  }
+  GIVEN("an input: \"a1234\"") {
+    Source src = Source_new("a1234");
+    WHEN("apply string1(\"a123\")") {
+      THEN("results \"a123\"") {
+        REQUIRE("a123" == parse(string1("a123"), src));
+      }
+    }
+  }
+  GIVEN("an input: \"a123bc\"") {
+    Source src = Source_new("a123bc");
+    WHEN("apply string1(\"a123\")") {
+      THEN("results \"a123\"") {
+        REQUIRE("a123" == parse(string1("a123"), src));
+      }
+    }
+  }
+  GIVEN("an input: \"a123bc\"") {
+    Source src = Source_new("a123bc");
+    WHEN("apply string1(\"a1234\")") {
+      THEN("cause exception(\"expects '4' but was 'b'\")") {
+        REQUIRE_THROWS_WITH(parse(string1("a1234"), src),
+                            "expects '4' but was 'b'");
+      }
+    }
+  }
+  GIVEN("an input: \"a123\"") {
+    Source src = Source_new("a123");
+    WHEN("apply string1(\"a1234\")") {
+      THEN("cause exception(\"too short\")") {
+        REQUIRE_THROWS_WITH(parse(string1("a1234"), src), "too short");
+      }
+    }
+  }
+  cparsec2_end();
+}
