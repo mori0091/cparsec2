18ee1da5565f422b5e708af70e016a21a65ad4a0
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-05-27 21:20:08 +0900

    added new test SCENARIO("anyChar")

---
+++ test/src/test_anyChar.cpp
@@ -0,0 +1,73 @@
+/* -*- coding: utf-8-unix -*- */
+
+#include <catch.hpp>
+#include <cparsec2.hpp>
+
+SCENARIO("anyChar", "[cparsec2][parser]") {
+  cparsec2_init();
+  GIVEN("an input \"abc\" ") {
+    Source src = Source_new("abc");
+    WHEN("apply anyChar") {
+      THEN("results 'a'") { REQUIRE('a' == parse(anyChar, src)); }
+    }
+    WHEN("apply anyChar") {
+      AND_WHEN("apply anyChar") {
+        THEN("results 'a'") {
+          REQUIRE('a' == parse(anyChar, src));
+          AND_THEN("results 'b'") {
+            REQUIRE('b' == parse(anyChar, src));
+          }
+        }
+      }
+    }
+    WHEN("apply anyChar") {
+      AND_WHEN("apply anyChar") {
+        THEN("results 'a'") {
+          REQUIRE('a' == parse(anyChar, src));
+          AND_THEN("results 'b'") {
+            REQUIRE('b' == parse(anyChar, src));
+            AND_THEN("results 'c'") {
+              REQUIRE('c' == parse(anyChar, src));
+            }
+          }
+        }
+      }
+    }
+    WHEN("apply anyChar") {
+      AND_WHEN("apply anyChar") {
+        AND_WHEN("apply anyChar") {
+          THEN("results 'a'") {
+            REQUIRE('a' == parse(anyChar, src));
+            AND_THEN("results 'b'") {
+              REQUIRE('b' == parse(anyChar, src));
+              AND_THEN("results 'c'") {
+                REQUIRE('c' == parse(anyChar, src));
+              }
+            }
+          }
+        }
+      }
+    }
+    WHEN("apply anyChar") {
+      AND_WHEN("apply anyChar") {
+        AND_WHEN("apply anyChar") {
+          AND_WHEN("apply anyChar") {
+            THEN("results 'a'") {
+              REQUIRE('a' == parse(anyChar, src));
+              AND_THEN("results 'b'") {
+                REQUIRE('b' == parse(anyChar, src));
+                AND_THEN("results 'c'") {
+                  REQUIRE('c' == parse(anyChar, src));
+                  AND_THEN("cause exception(\"too short\")") {
+                    REQUIRE_THROWS_WITH(parse(anyChar, src), "too short");
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  cparsec2_end();
+}
