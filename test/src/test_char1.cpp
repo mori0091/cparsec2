a507cbcd6fe118fef8bdfa09b5ef39431cc9e504
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-05-27 21:39:28 +0900

    added test SCENARIO("char1(c)")

---
+++ test/src/test_char1.cpp
@@ -0,0 +1,29 @@
+/* -*- coding: utf-8-unix -*- */
+
+#include <catch.hpp>
+#include <cparsec2.hpp>
+
+SCENARIO("char1(c)", "[cparsec2][parser]") {
+  cparsec2_init();
+  GIVEN("an input: \"abc\" ") {
+    Source src = Source_new("abc");
+    WHEN("apply char1('a')") {
+      THEN("results 'a'") { REQUIRE('a' == parse(char1('a'), src)); }
+    }
+    WHEN("apply char1('b')") {
+      THEN("cause exception(\"expects 'b' but was 'a'\")") {
+        REQUIRE_THROWS_WITH(parse(char1('b'), src),
+                            "expects 'b' but was 'a'");
+      }
+    }
+  }
+  GIVEN("an input: \"\"") {
+    Source src = Source_new("");
+    WHEN("apply char1('a')") {
+      THEN("cause exception(\"too short\")") {
+        REQUIRE_THROWS_WITH(parse(char1('a'), src), "too short");
+      }
+    }
+  }
+  cparsec2_end();
+}
