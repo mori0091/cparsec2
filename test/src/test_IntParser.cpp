3366b266cbdb048bd862f2257b4232782c14c8eb
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-05-30 23:31:08 +0900

    fixed test SCENARIO("PARSER(Int) number")

--- test/src/test_IntParser.cpp
+++ test/src/test_IntParser.cpp
@@ -23,15 +23,17 @@ SCENARIO("PARSER(Int) number", "[cparsec2][parser][IntParser]") {
     Source src = Source_new("100+200");
     WHEN("apply number -> x") {
       int x = parse(number, src);
-      THEN("results x = 100") { REQUIRE(x == 100); }
-      AND_WHEN("apply char1('+')") {
-        THEN("results '+'") {
-          REQUIRE('+' == parse(char1('+'), src));
-          AND_WHEN("apply number -> y") {
-            int y = parse(number, src);
-            THEN("results y = 200") {
-              REQUIRE(y == 200);
-              AND_THEN("x + y = 300") { REQUIRE(x + y == 300); }
+      THEN("results x = 100") {
+        REQUIRE(x == 100);
+        AND_WHEN("apply char1('+')") {
+          THEN("results '+'") {
+            REQUIRE('+' == parse(char1('+'), src));
+            AND_WHEN("apply number -> y") {
+              int y = parse(number, src);
+              THEN("results y = 200") {
+                REQUIRE(y == 200);
+                AND_THEN("x + y = 300") { REQUIRE(x + y == 300); }
+              }
             }
           }
         }
