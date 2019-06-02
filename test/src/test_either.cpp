ed99d96edcd7d2601acfb07849ab78cc9586341a
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-06-02 12:48:48 +0900

    added some testcase to the test SCENARIO("either(p1,p2)") (#1)

--- test/src/test_either.cpp
+++ test/src/test_either.cpp
@@ -3,8 +3,9 @@
 #include <catch.hpp>
 #include <cparsec2.hpp>
 
-SCENARIO("either(string1(\"ab\"), string1(\"bc\"))", "[cparsec2][parser][either]") {
+SCENARIO("either(p1, p2)", "[cparsec2][parser][either]") {
   cparsec2_init();
+
   GIVEN("PARSER(String) p = either(string1(\"ab\"), string1(\"bc\"))") {
     PARSER(String) p = either(string1("ab"), string1("bc"));
     WHEN("applied to an input: \"ab\"") {
@@ -38,5 +39,68 @@ SCENARIO("either(string1(\"ab\"), string1(\"bc\"))", "[cparsec2][parser][either]
       }
     }
   }
+
+  GIVEN("PARSER(String) p = either(\"ab\", \"bc\")") {
+    PARSER(String) p = either("ab", "bc");
+    WHEN("applied to an input: \"ab\"") {
+      Source src = Source_new("ab");
+      THEN("results \"ab\"") {
+        REQUIRE("ab" == parse(p, src));
+      }
+    }
+    WHEN("applied to an input: \"bc\"") {
+      Source src = Source_new("bc");
+      THEN("results \"bc\"") {
+        REQUIRE("bc" == parse(p, src));
+      }
+    }
+    WHEN("applied to an input: \"ac\"") {
+      Source src = Source_new("ac");
+      THEN("causes an error \"expects 'b' but was 'c'\"") {
+        REQUIRE_THROWS_WITH(parse(p, src), "expects 'b' but was 'c'");
+      }
+    }
+    WHEN("applied to an input: \"bd\"") {
+      Source src = Source_new("bd");
+      THEN("causes an error \"expects 'c' but was 'd'\"") {
+        REQUIRE_THROWS_WITH(parse(p, src), "expects 'c' but was 'd'");
+      }
+    }
+    WHEN("applied to an input: \"b\"") {
+      Source src = Source_new("b");
+      THEN("causes an error \"too short\"") {
+        REQUIRE_THROWS_WITH(parse(p, src), "too short");
+      }
+    }
+  }
+
+  GIVEN("PARSER(Char) p = either('a', 'b')") {
+    PARSER(Char) p = either('a', 'b');
+    WHEN("applied to an input: \"ab\"") {
+      Source src = Source_new("ab");
+      THEN("results 'a'") {
+        REQUIRE('a' == parse(p, src));
+      }
+    }
+    WHEN("applied to an input: \"bc\"") {
+      Source src = Source_new("bc");
+      THEN("results 'b'") {
+        REQUIRE('b' == parse(p, src));
+      }
+    }
+    WHEN("applied to an input: \"c\"") {
+      Source src = Source_new("c");
+      THEN("causes an error \"expects 'b' but was 'c'\"") {
+        REQUIRE_THROWS_WITH(parse(p, src), "expects 'b' but was 'c'");
+      }
+    }
+    WHEN("applied to an input: \"\"") {
+      Source src = Source_new("");
+      THEN("causes an error \"too short\"") {
+        REQUIRE_THROWS_WITH(parse(p, src), "too short");
+      }
+    }
+  }
+
   cparsec2_end();
 }
