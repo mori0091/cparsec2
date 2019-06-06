c8e08323f688821cc8413c69704611381c9eb76b
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-06-06 21:05:11 +0900

    added tests for 'example/calc' by overriding TEST_COMMAND make-variable.

--- example/calc/.project.mk
+++ example/calc/.project.mk
@@ -7,6 +7,11 @@ CXXFLAGS +=
 LDFLAGS  += -L ../../lib
 LDLIBS   += -lcparsec2
 
+TEST_COMMAND = \
+	./$(TARGET) && \
+	./$(TARGET) '3 * (1 + 2) -1' && \
+	./$(TARGET) '1 + 2  ;' || true
+
 $(TARGET): ../../lib/libcparsec2.a
 
 ../../lib/libcparsec2.a: depend
