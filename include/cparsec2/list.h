e5d55a52e2c6b2cdcb3271296d5e5fc9c45e7b23
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-06-14 19:18:37 +0900

    added List(T) and Buff(T) - a generic container and a list-builder

---
+++ include/cparsec2/list.h
@@ -0,0 +1,110 @@
+/* -*- coding:utf-8-unix -*- */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ---- List ----
+#define List(T) T##List
+#define LIST_BEGIN(T) list_begin_##T
+#define LIST_END(T) list_end_##T
+#define LIST_LENGTH(T) list_length_##T
+
+#define TYPEDEF_LIST(T, E)                                               \
+  typedef struct {                                                       \
+    E* data;                                                             \
+    int len;                                                             \
+  } List(T)
+
+#define DECLARE_LIST(T, E)                                               \
+  E* LIST_BEGIN(T)(List(T) xs);                                          \
+  E* LIST_END(T)(List(T) xs);                                            \
+  int LIST_LENGTH(T)(List(T) xs)
+
+// List(Char)
+#define CharList String
+typedef char* List(Char);
+DECLARE_LIST(Char, char);
+
+// List(String)
+TYPEDEF_LIST(String, const char*);
+DECLARE_LIST(String, const char*);
+
+// List(Int)
+TYPEDEF_LIST(Int, int);
+DECLARE_LIST(Int, int);
+
+// clang-format off
+#define list_begin(xs)                          \
+  _Generic((xs)                                 \
+           , List(Char)   : LIST_BEGIN(Char)    \
+           , List(String) : LIST_BEGIN(String)  \
+           , List(Int)    : LIST_BEGIN(Int)     \
+           )(xs)
+#define list_end(xs)                            \
+  _Generic((xs)                                 \
+           , List(Char)   : LIST_END(Char)      \
+           , List(String) : LIST_END(String)    \
+           , List(Int)    : LIST_END(Int)       \
+           )(xs)
+#define list_length(xs)                         \
+  _Generic((xs)                                 \
+           , List(Char)   : LIST_LENGTH(Char)   \
+           , List(String) : LIST_LENGTH(String) \
+           , List(Int)    : LIST_LENGTH(Int)    \
+           )(xs)
+// clang-format on
+
+// ---- Buffer (List builder) ----
+#define Buff(T) T##Buff
+#define BUFF_ENSURE(T) buff_ensure_##T
+#define BUFF_PUSH(T) buff_push_##T
+#define BUFF_APPEND(T) buff_append_##T
+#define BUFF_FINISH(T) buff_finish_##T
+
+#define DECLARE_BUFF(T, E)                                               \
+  typedef struct {                                                       \
+    E* data;                                                             \
+    int capacity;                                                        \
+    int len;                                                             \
+  } Buff(T);                                                             \
+  void BUFF_ENSURE(T)(Buff(T) * b);                                      \
+  void BUFF_PUSH(T)(Buff(T) * b, E x);                                   \
+  void BUFF_APPEND(T)(Buff(T) * b, List(T) xs);                          \
+  List(T) BUFF_FINISH(T)(Buff(T) * b)
+
+DECLARE_BUFF(Char, char);
+DECLARE_BUFF(String, const char*);
+DECLARE_BUFF(Int, int);
+
+// clang-format off
+#define buff_ensure(b)                          \
+  _Generic((*(b))                               \
+           , Buff(Char)   : BUFF_ENSURE(Char)   \
+           , Buff(String) : BUFF_ENSURE(String) \
+           , Buff(Int)    : BUFF_ENSURE(Int)    \
+           )(b)
+#define buff_push(b, x)                         \
+  _Generic((*(b))                               \
+           , Buff(Char)   : BUFF_PUSH(Char)     \
+           , Buff(String) : BUFF_PUSH(String)   \
+           , Buff(Int)    : BUFF_PUSH(Int)      \
+           )(b, x)
+#define buff_append(b, xs)                      \
+  _Generic((*(b))                               \
+           , Buff(Char)   : BUFF_APPEND(Char)   \
+           , Buff(String) : BUFF_APPEND(String) \
+           , Buff(Int)    : BUFF_APPEND(Int)    \
+           )(b, xs)
+#define buff_finish(b)                          \
+  _Generic((*(b))                               \
+           , Buff(Char)   : BUFF_FINISH(Char)   \
+           , Buff(String) : BUFF_FINISH(String) \
+           , Buff(Int)    : BUFF_FINISH(Int)    \
+           )(b)
+// clang-format on
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/list.c
@@ -0,0 +1,88 @@
+/* -*- coding: utf-8-unix */
+
+#include <assert.h>
+#include <cparsec2.h>
+
+// ---- List ----
+
+#define DEFINE_LIST(T, E)                                                \
+  E* LIST_BEGIN(T)(List(T) xs) {                                         \
+    return xs.data;                                                      \
+  }                                                                      \
+  E* LIST_END(T)(List(T) xs) {                                           \
+    return xs.data + xs.len;                                             \
+  }                                                                      \
+  int LIST_LENGTH(T)(List(T) xs) {                                       \
+    return xs.len;                                                       \
+  }                                                                      \
+  _Static_assert(1, "")
+
+// List(String)
+DEFINE_LIST(String, const char*);
+
+// List(Int)
+DEFINE_LIST(Int, int);
+
+// List(Char) ; a special case of List(T)
+char* LIST_BEGIN(Char)(List(Char) xs) {
+  assert(xs);
+  return xs;
+}
+char* LIST_END(Char)(List(Char) xs) {
+  assert(xs);
+  return xs + strlen(xs);
+}
+int LIST_LENGTH(Char)(List(Char) xs) {
+  assert(xs);
+  return strlen(xs);
+}
+
+// ---- Buffer (List builder) ----
+
+#define DEFINE_BUFF(T, E)                                                \
+  DEFINE_BUFF_COMMON(T, E);                                              \
+  DEFINE_BUFF_FINISH(T)
+
+#define DEFINE_BUFF_COMMON(T, E)                                         \
+  void BUFF_ENSURE(T)(Buff(T) * b) {                                     \
+    if (!b->data) {                                                      \
+      b->data = mem_malloc(sizeof(E) * 8);                               \
+      b->capacity = 8;                                                   \
+      b->len = 0;                                                        \
+    }                                                                    \
+    if (b->capacity == b->len) {                                         \
+      b->capacity *= 2;                                                  \
+      b->data = mem_realloc(b->data, sizeof(E) * b->capacity);           \
+    }                                                                    \
+  }                                                                      \
+  void BUFF_PUSH(T)(Buff(T) * b, E x) {                                  \
+    BUFF_ENSURE(T)(b);                                                   \
+    b->data[b->len++] = x;                                               \
+  }                                                                      \
+  void BUFF_APPEND(T)(Buff(T) * b, List(T) xs) {                         \
+    E* itr = LIST_BEGIN(T)(xs);                                          \
+    E* end = LIST_END(T)(xs);                                            \
+    for (; itr != end; itr++) {                                          \
+      BUFF_PUSH(T)(b, *itr);                                             \
+    }                                                                    \
+  }                                                                      \
+  _Static_assert(1, "")
+
+#define DEFINE_BUFF_FINISH(T)                                            \
+  List(T) BUFF_FINISH(T)(Buff(T) * b) {                                  \
+    return (List(T)){.data = b->data, .len = b->len};                    \
+  }                                                                      \
+  _Static_assert(1, "")
+
+// Buff(String)
+DEFINE_BUFF(String, const char*);
+
+// Buff(Int)
+DEFINE_BUFF(Int, int);
+
+// Buff(Char) ; a special case of Buff(T)
+DEFINE_BUFF_COMMON(Char, char);
+List(Char) BUFF_FINISH(Char)(Buff(Char) * b) {
+  BUFF_PUSH(Char)(b, '\0');
+  return (List(Char))b->data;
+}
