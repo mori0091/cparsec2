17a135a8764134263723b0a6523818ca7548a9af
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-06-15 23:36:57 +0900

    added tests for `cons(p,ps)`

--- test/src/test_cons.cpp
+++ test/src/test_cons.cpp
@@ -40,3 +40,30 @@ SCENARIO("cons(letter, many(digit))", "[cparsec2][parser][cons]") {
   }
   cparsec2_end();
 }
+
+SCENARIO("cons(p, ps)", "[cparsec2][parser][cons]") {
+  cparsec2_init();
+  GIVEN("an input: \"123 456 789\"") {
+    Source src = Source_new("123 456 789");
+    WHEN("apply cons(number, many(number))") {
+      List(Int) xs = parse(cons(number, many(number)), src);
+      THEN("results [123, 456, 789]") {
+        int* itr = list_begin(xs);
+        REQUIRE(123 == itr[0]);
+        REQUIRE(456 == itr[1]);
+        REQUIRE(789 == itr[2]);
+      }
+    }
+    WHEN("apply cons(x, many(x)) where x = token(many1(digit))") {
+      PARSER(String) x = token(many1(digit));
+      List(String) xs = parse(cons(x, many(x)), src);
+      THEN("results [\"123\", \"456\", \"789\"]") {
+        const char** itr = list_begin(xs);
+        REQUIRE("123" == std::string(itr[0]));
+        REQUIRE("456" == std::string(itr[1]));
+        REQUIRE("789" == std::string(itr[2]));
+      }
+    }
+  }
+  cparsec2_end();
+}
