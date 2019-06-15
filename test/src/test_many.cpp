2d076437e8da86813981717910bc30e46fd17c8e
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-06-15 13:02:05 +0900

    added tests for many(p), in case of the list-parser results to be empty list

--- test/src/test_many.cpp
+++ test/src/test_many.cpp
@@ -46,20 +46,36 @@ SCENARIO("many(p)", "[cparsec2][parser][many]") {
       }
     }
   }
+  GIVEN("an input \"abc\"") {
+    Source src = Source_new("abc");
+    WHEN("apply many(number)") {
+      List(Int) xs = parse(many(number), src);
+      THEN("results []") {
+        REQUIRE(list_begin(xs) == list_end(xs));
+      }
+    }
+    WHEN("apply many(token(many1(digit)))") {
+      List(String) xs = parse(many(token(many1(digit))), src);
+      THEN("results []") {
+        REQUIRE(list_begin(xs) == list_end(xs));
+      }
+    }
+  }
   GIVEN("an input \",123,456,789\"") {
     Source src = Source_new(",123,456,789");
     WHEN("apply many(skip1st(char1(','), number))") {
       List(Int) xs = parse(many(skip1st(char1(','), number)), src);
-      THEN("results \"[123, 456, 789]\"") {
+      THEN("results [123, 456, 789]") {
         int* itr = list_begin(xs);
         REQUIRE(123 == itr[0]);
         REQUIRE(456 == itr[1]);
         REQUIRE(789 == itr[2]);
       }
     }
-    WHEN("apply many(skip1st(char1(','), many1(\"digit\")))") {
-      List(String) xs = parse(many(skip1st(char1(','), many1(digit))), src);
-      THEN("results \"[\"123\", \"456\", \"789\"]\"") {
+    WHEN("apply many(skip1st(char1(','), many1(digit)))") {
+      List(String) xs =
+          parse(many(skip1st(char1(','), many1(digit))), src);
+      THEN("results [\"123\", \"456\", \"789\"]") {
         const char** itr = list_begin(xs);
         REQUIRE("123" == std::string(itr[0]));
         REQUIRE("456" == std::string(itr[1]));
