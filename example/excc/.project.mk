4b4d192c0a680f294921be85cca4821f4d99c660
Daishi Mori <mori-d@qc4.so-net.ne.jp>
2019-07-16 22:28:51 +0900

    added example/excc ; An experimental C compiler subset (#62)
    * added example/excc ; An experimental C compiler subset
    
    * added '+x' (executable) permission to example/excc/tests.sh

---
+++ example/excc/.project.mk
@@ -0,0 +1,15 @@
+# -*- coding:utf-8-unix -*-
+TARGET    = bin/excc
+
+CFLAGS   += -std=c11 -I ../../include -I include -I .
+LDFLAGS  += -L ../../lib
+LDLIBS   += -lcparsec2
+
+TEST_COMMAND = ./tests.sh
+
+$(TARGET): ../../lib/libcparsec2.a
+
+../../lib/libcparsec2.a: depend
+
+depend:
+	$(MAKE) lib -C ../..
