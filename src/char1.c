e0acf73077e41d95d26ed48f17f3b17cc69bc9ad
Daishi Mori <mori-d@qc4.so-net.ne.jp>
2019-09-08 13:54:27 +0900

    Feature/char1 cache (#75)
    * implemented cache for reusing 'char1(c)' parsers
    
    * added control character representation used in error-message of 'char1(c)'

--- src/char1.c
+++ src/char1.c
@@ -2,18 +2,50 @@
 
 #include "cparsec2.h"
 
+#include <ctype.h>
+
+struct char1_cache {
+  PARSER(Char) parser[256];
+  const char* str[256];
+};
+
+static struct char1_cache char1_cache = {0};
+
+void cparsec2_clear_char1_cache(void) {
+  char1_cache = (struct char1_cache){0};
+}
+
 static const char* c2s(uint8_t c) {
   switch (c) {
-  case '\n':
-    return "'\\n'";
-  case '\r':
-    return "'\\r'";
-  case '\t':
+  case '\0':                    /* Null */
+    return "'\\0'";
+  case '\a':                    /* Bell */
+    return "'\\a'";
+  case '\b':                    /* Backspace */
+    return "'\\b'";
+  case '\t':                    /* Horizontal Tab */
     return "'\\t'";
-  case ' ':
-    return "' '";
-  default:
-    return mem_printf("'%c'", c);
+  case '\n':                    /* Line Feed */
+    return "'\\n'";
+  case '\v':                    /* Vertical Tab */
+    return "'\\v'";
+  case '\f':                    /* Form Feed */
+    return "'\\f'";
+  case '\r':                    /* Carriage Return */
+    return "'\\r'";
+  case '\\':                    /* Backslash */
+    return "'\\\\'";
+  default:;
+    const char** p = &(char1_cache.str[c]);
+    if (!*p) {
+      if (isprint(c)) {
+        *p = mem_printf("'%c'", c);
+      }
+      else {
+        *p = mem_printf("'\\%03o'", (unsigned int)c);
+      }
+    }
+    return *p;
   }
 }
 
@@ -29,5 +61,9 @@ static char run_char1(void* arg, Source src, Ctx* ex) {
 }
 
 PARSER(Char) char1(char c) {
-  return expects(c2s(c), PARSER_GEN(Char)(run_char1, (void*)(intptr_t)c));
+  PARSER(Char)* p = &(char1_cache.parser[(uint8_t)c]);
+  if (!*p) {
+    *p = expects(c2s(c), PARSER_GEN(Char)(run_char1, (void*)(intptr_t)c));
+  }
+  return *p;
 }
