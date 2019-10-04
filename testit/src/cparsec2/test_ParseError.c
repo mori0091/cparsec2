b5010b0e632a157834fe15233ed944c12a549eb9
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-10-04 21:34:49 +0900

    fixed test_ParseError.c; lacks cparsec2_init()/cparsec2_end() call

--- testit/src/cparsec2/test_ParseError.c
+++ testit/src/cparsec2/test_ParseError.c
@@ -4,16 +4,19 @@
 #include <cparsec2.h>
 
 test("a ParseError w/ no messages is 'unknown error'") {
+  cparsec2_init();
   ParseError e = ParseError_new();
   c_assert(ParseError_isUnknown(e));
   c_assert(eq(0, e.offset));
   c_assert(eq(1, e.pos.line));
   c_assert(eq(1, e.pos.column));
   c_assert(eq(0, LIST_LENGTH(ErrMsg)(ParseError_getMessages(e))));
+  cparsec2_end();
 }
 
 test("ParseError_addMessage(msg, e) creates a new ParseError "
      "that has all messages in 'e' and the given 'msg'") {
+  cparsec2_init();
   ParseError e0, e1, e2, e3;
   e0 = ParseError_new();
   e1 = ParseError_addMessage((ErrMsg){Unexpect, "a message #1"}, e0);
@@ -23,21 +26,25 @@ test("ParseError_addMessage(msg, e) creates a new ParseError "
   c_assert(eq(1, LIST_LENGTH(ErrMsg)(ParseError_getMessages(e1))));
   c_assert(eq(2, LIST_LENGTH(ErrMsg)(ParseError_getMessages(e2))));
   c_assert(eq(3, LIST_LENGTH(ErrMsg)(ParseError_getMessages(e3))));
+  cparsec2_end();
 }
 
 test("ParseError_setMessage(msg, e) creates a new ParseError "
      "that has the given 'msg'.") {
+  cparsec2_init();
   ParseError e = ParseError_new();
   e = ParseError_setMessage((ErrMsg){Expect, "foo"}, e);
   List(ErrMsg) msgs = ParseError_getMessages(e);
   c_assert(eq(1, LIST_LENGTH(ErrMsg)(msgs)));
   c_assert(eq(Expect, LIST_BEGIN(ErrMsg)(msgs)[0].type));
   c_assert(eq("foo", LIST_BEGIN(ErrMsg)(msgs)[0].msg));
+  cparsec2_end();
 }
 
 test("ParseError_setMessage(msg, e) creates a new ParseError, "
      "that contains 'msg' and any messages in 'e', "
      "of type differ from 'msg'.") {
+  cparsec2_init();
   ParseError e1, e2;
   e1 = ParseError_new();
   e1 = ParseError_addMessage((ErrMsg){Unexpect, "foo"}, e1);
@@ -52,4 +59,5 @@ test("ParseError_setMessage(msg, e) creates a new ParseError, "
   c_assert(eq(Expect, LIST_BEGIN(ErrMsg)(msgs)[1].type));
   c_assert(eq("FOO", LIST_BEGIN(ErrMsg)(msgs)[0].msg));
   c_assert(eq("baz", LIST_BEGIN(ErrMsg)(msgs)[1].msg));
+  cparsec2_end();
 }
