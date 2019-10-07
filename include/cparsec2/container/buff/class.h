ad5999769f2fbe1bb78e3843da6576b97343ee30
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-10-07 23:20:21 +0900

    added buff_ensure_by(b, sz) API and refactored Buff(T)

--- include/cparsec2/container/buff/class.h
+++ include/cparsec2/container/buff/class.h
@@ -6,6 +6,7 @@
 #define Buff(T) CAT(T, Buff)
 
 // Name of functions for Buff(T)
+#define BUFF_ENSURE_BY(T) CAT(buff_ensure_by_, T)
 #define BUFF_ENSURE(T) CAT(buff_ensure_, T)
 #define BUFF_PUSH(T) CAT(buff_push_, T)
 #define BUFF_APPEND(T) CAT(buff_append_, T)
@@ -23,6 +24,7 @@
 
 // Declares function prototypes for Buff(T)
 #define DECLARE_BUFF(T)                                                  \
+  void BUFF_ENSURE_BY(T)(Buff(T) * b, size_t sz);                        \
   void BUFF_ENSURE(T)(Buff(T) * b);                                      \
   void BUFF_PUSH(T)(Buff(T) * b, ELEMENT_TYPE(Buff(T)) x);               \
   void BUFF_APPEND(T)(Buff(T) * b, List(T) xs);                          \
@@ -33,17 +35,25 @@
   DEFINE_BUFF_FINISH(T)
 
 #define DEFINE_BUFF_COMMON(T)                                            \
-  void BUFF_ENSURE(T)(Buff(T) * b) {                                     \
+  void BUFF_ENSURE_BY(T)(Buff(T) * b, size_t sz) {                       \
+    const size_t elem_size = sizeof(ELEMENT_TYPE(Buff(T)));              \
     if (!b->data) {                                                      \
-      b->data = mem_malloc(sizeof(ELEMENT_TYPE(Buff(T))) * 8);           \
-      b->capacity = 8;                                                   \
-      b->len = 0;                                                        \
+      int cap = (sz + 7) / 8 * 8;                                        \
+      if (0 < cap) {                                                     \
+        b->len = 0;                                                      \
+        b->capacity = cap;                                               \
+        b->data = mem_malloc(elem_size * cap);                           \
+      }                                                                  \
+    } else {                                                             \
+      int cap = b->len + sz;                                             \
+      if (b->capacity < cap) {                                           \
+        b->capacity = cap;                                               \
+        b->data = mem_realloc(b->data, elem_size * cap);                 \
+      }                                                                  \
     }                                                                    \
-    if (b->capacity == b->len) {                                         \
-      b->capacity *= 2;                                                  \
-      b->data = mem_realloc(b->data, sizeof(ELEMENT_TYPE(Buff(T))) *     \
-                                         b->capacity);                   \
-    }                                                                    \
+  }                                                                      \
+  void BUFF_ENSURE(T)(Buff(T) * b) {                                     \
+    BUFF_ENSURE_BY(T)(b, 1);                                             \
   }                                                                      \
   void BUFF_PUSH(T)(Buff(T) * b, ELEMENT_TYPE(Buff(T)) x) {              \
     BUFF_ENSURE(T)(b);                                                   \
@@ -51,9 +61,10 @@
   }                                                                      \
   void BUFF_APPEND(T)(Buff(T) * b, List(T) xs) {                         \
     ELEMENT_TYPE(List(T))* itr = LIST_BEGIN(T)(xs);                      \
-    ELEMENT_TYPE(List(T))* end = LIST_END(T)(xs);                        \
-    for (; itr != end; itr++) {                                          \
-      BUFF_PUSH(T)(b, *itr);                                             \
+    int n = LIST_LENGTH(T)(xs);                                          \
+    BUFF_ENSURE_BY(T)(b, n);                                             \
+    for (int i = 0; i < n; ++i) {                                        \
+      b->data[b->len++] = *itr++;                                        \
     }                                                                    \
   }                                                                      \
   END_OF_STATEMENTS
