b1b15f8c72850874b21d34fce3a625a664d63739
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-10-08 00:05:49 +0900

    refactored mem_printf() API by using buff_vprintf()

--- src/alloc.c
+++ src/alloc.c
@@ -105,23 +105,10 @@ void mem_free(void* p) {
 }
 
 const char* mem_printf(const char* fmt, ...) {
-  int len;
+  Buff(Char) b = {0};
   va_list ap;
   va_start(ap, fmt);
-  len = vsnprintf(NULL, 0, fmt, ap);
+  buff_vprintf(&b, fmt, ap);
   va_end(ap);
-  if (len < 0) {
-    fprintf(stderr, "vsnprintf(NULL, 0, fmt, ...):%s\n", strerror(errno));
-    exit(1);
-  }
-  char* buf = mem_malloc(len + 1);
-  va_start(ap, fmt);
-  len = vsnprintf(buf, len + 1, fmt, ap);
-  va_end(ap);
-  if (len < 0) {
-    fprintf(stderr, "vsnprintf(buf, len, fmt, ...):%s\n",
-            strerror(errno));
-    exit(1);
-  }
-  return buf;
+  return buff_finish(&b);
 }
