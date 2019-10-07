389d3f1f43bd06a982946d08688e6dd41bdb9568
mori0091 <mori-d@qc4.so-net.ne.jp>
2019-10-08 00:04:18 +0900

    added void buff_printf(Buff(Char)* b, const char* format, ...) API

--- include/cparsec2/container/buff.h
+++ include/cparsec2/container/buff.h
@@ -9,3 +9,26 @@
 #include "buff/prototype.h"
 
 #include "buff/generics.h"
+
+#include <stdarg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /**
+   * \brief Appends a printf-like formatted string.
+   *
+   * \param b       pointer to a char buffer
+   * \param format  format string
+   * \param ...     arguments for formatting
+   *
+   * \see printf(3)
+   */
+  void buff_printf(Buff(Char) * b, const char* format, ...);
+
+  void buff_vprintf(Buff(Char) * b, const char* format, va_list ap);
+
+#ifdef __cplusplus
+}
+#endif
