e2a8449db73ca978c9628efbc0557577899617c3
Daishi Mori <mori-d@qc4.so-net.ne.jp>
2019-10-17 23:00:54 +0900

    fixed: some combinators had not defined for user-typeset (#103)

--- include/cparsec2/typeset.h
+++ include/cparsec2/typeset.h
@@ -12,48 +12,48 @@
 #ifdef CPARSEC2_USER_TYPESET
 
 #define CPARSEC2_USER_TYPESET_I() CPARSEC2_USER_TYPESET
+
 #define CPARSEC2_DEFINE_USER_TYPESET()                                   \
-  FOREACH(CPARSEC2_DEFINE_CONTAINER, SQUASH(CPARSEC2_USER_TYPESET));     \
-  FOREACH(CPARSEC2_DEFINE_PARSER_CLASS, SQUASH(CPARSEC2_USER_TYPESET));  \
-  FOREACH(CPARSEC2_DEFINE_COMBINATORS, SQUASH(CPARSEC2_USER_TYPESET))
+  CPARSEC2_C_API_BEGIN                                                   \
+  FOREACH(DEFINE_LIST, CPARSEC2_USER_TYPESET_0());                       \
+  FOREACH(DEFINE_BUFF, CPARSEC2_USER_TYPESET_0());                       \
+  FOREACH(DEFINE_SHOW_LIST, CPARSEC2_USER_TYPESET_0());                  \
+  FOREACH(DEFINE_PARSER, CPARSEC2_USER_TYPESET_1());                     \
+  FOREACH(DEFINE_RUNPARSER, CPARSEC2_USER_TYPESET_1());                  \
+  FOREACH(CPARSEC2_DEFINE_COMBINATORS_0, CPARSEC2_USER_TYPESET_0());     \
+  FOREACH(CPARSEC2_DEFINE_COMBINATORS_1, CPARSEC2_USER_TYPESET_1());     \
+  CPARSEC2_C_API_END                                                     \
+  END_OF_STATEMENTS
+
+#define CPARSEC2_USER_TYPESET_0() SQUASH(CPARSEC2_USER_TYPESET)
+#define CPARSEC2_USER_TYPESET_1()                                        \
+  CPARSEC2_USER_TYPESET_0(), APPLY(List, CPARSEC2_USER_TYPESET_0())
 
 #else
 
 #define CPARSEC2_USER_TYPESET_I()
+
 #define CPARSEC2_DEFINE_USER_TYPESET()                                   \
   static_assert(0, "CPARSEC2_USER_TYPESET is not defined.")
 
 #endif
 
-#define CPARSEC2_DEFINE_CONTAINER(T)                                     \
-  CPARSEC2_C_API_BEGIN                                                   \
-  DEFINE_LIST(T);                                                        \
-  DEFINE_BUFF(T);                                                        \
-  CPARSEC2_C_API_END                                                     \
-  END_OF_STATEMENTS
-
-#define CPARSEC2_DEFINE_PARSER_CLASS(T)                                  \
-  CPARSEC2_C_API_BEGIN                                                   \
-  DEFINE_PARSER(T);                                                      \
-  DEFINE_RUNPARSER(T);                                                   \
-  DEFINE_SHOW_LIST(T);                                                   \
-  DEFINE_PARSER(List(T));                                                \
-  DEFINE_RUNPARSER(List(T));                                             \
-  CPARSEC2_C_API_END                                                     \
-  END_OF_STATEMENTS
-
-#define CPARSEC2_DEFINE_COMBINATORS(T)                                   \
-  CPARSEC2_C_API_BEGIN                                                   \
-  DEFINE_EXPECTS(T);                                                     \
+// Parser-combinators which
+// - takes a PARSER(T) and constructs a PARSER(List(T))
+#define CPARSEC2_DEFINE_COMBINATORS_0(T)                                 \
   DEFINE_MANY(T);                                                        \
   DEFINE_MANY1(T);                                                       \
   DEFINE_CONS(T);                                                        \
-  DEFINE_SEQ(T);                                                         \
+  DEFINE_SEQ(T)
+
+// Parser-combinators which
+// - takes a PARSER(T) and constructs a PARSER(T), or
+// - takes a PARSER(List(T)) and constructs a PARSER(List(T))
+#define CPARSEC2_DEFINE_COMBINATORS_1(T)                                 \
+  DEFINE_EXPECTS(T);                                                     \
   DEFINE_SKIP(T);                                                        \
   DEFINE_SKIP1ST(T);                                                     \
   DEFINE_SKIP2ND(T);                                                     \
   DEFINE_EITHER(T);                                                      \
   DEFINE_TRYP(T);                                                        \
-  DEFINE_TOKEN(T);                                                       \
-  CPARSEC2_C_API_END                                                     \
-  END_OF_STATEMENTS
+  DEFINE_TOKEN(T)
